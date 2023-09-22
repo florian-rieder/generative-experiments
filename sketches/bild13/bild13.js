d3984bad93497fc4defbd8d881d89e47819b7ff3
Sergenti <florian.rieder@unil.ch>
2023-09-22 18:23:15 +0200

    small improvements

--- sketches/bild13/bild13.js
+++ sketches/bild13/bild13.js
@@ -1,29 +1,40 @@
-const PHI = (1 + Math.sqrt(5)) / 2; // golden ratio
+// Define the golden ratio
+const PHI = (1 + Math.sqrt(5)) / 2;
+
+// Number of circles to draw
 let numCircles = 1000;
+
+// Base radius for the circles
 let baseRadius = 1000;
+
+// Margin around the canvas
 let margin = 50;
 
+// Setup function, runs once at the beginning
 function setup() {
-    createCanvas(windowWidth / PHI, windowHeight);
-    noFill();
-    strokeWeight(0.5);
-    translate(width / 2, height / 2); // center the origin
-    margin = height * 8 / 100;
-    drawBild();
+    createCanvas(windowWidth / PHI, windowHeight); // Create a canvas with dimensions
+    noFill(); // Don't fill shapes
+    strokeWeight(0.5); // Set stroke weight for lines
+    translate(width / 2, height / 2); // Center the origin
+    margin = height * 8 / 100; // Set margin based on canvas height
+    drawBild(); // Call the custom drawing function
 }
 
+// Function to draw the pattern
 function drawBild() {
-    background("white");
+    background("white"); // Set background color
 
     // Used to draw arcs on both sides
     const signs = [-1, 1];
 
+    // Loop through the number of circles
     for (let i = 0; i < numCircles; i++) {
         signs.forEach(sign => {
+            // Calculate properties for each circle
             let radius = Math.pow(baseRadius * i / numCircles, 1.5);
             let cx = radius * sign;
-            let cy = 0
-            let startAngle = 0
+            let cy = 0;
+            let startAngle = 0;
             let endAngle = TWO_PI;
 
             // Calculate the maximum angles possible within the margin
@@ -32,7 +43,9 @@ function drawBild() {
             let a = xMargin - radius;
             let b = yMargin * sign;
 
-            // Doesn't meet any margin
+            // Check different cases for arc angles based on margin
+
+            // No margin constraints
             if (radius * 2 < xMargin && radius < yMargin) {
                 if (sign > 0) {
                     startAngle = random(PI);
@@ -43,49 +56,47 @@ function drawBild() {
                 }
             }
 
-            // Horizontal margin
+            // Horizontal margin constraint
             if (radius * 2 > xMargin) {
                 let yIntersect = Math.sqrt(radius * radius - a * a);
                 let angle = Math.atan2(yIntersect, a);
                 if (sign > 0) {
-                    startAngle = random(angle, PI)
+                    startAngle = random(angle, PI);
                     endAngle = -startAngle;
                 } else {
                     endAngle = random(PI - angle);
                     startAngle = -endAngle;
                 }
-
             }
 
-            // Vertical margin
+            // Vertical margin constraint
             if (radius > yMargin) {
                 xIntersect = Math.sqrt(radius * radius - yMargin * yMargin);
                 let angle = Math.atan2(b, xIntersect);
                 if (sign > 0) {
                     startAngle = angle + 2 * ((PI / 2) - angle);
-                    startAngle = random(startAngle, PI)
+                    startAngle = random(startAngle, PI);
                     endAngle = random(PI + angle / 2, angle + PI);
                 } else {
                     startAngle = random(angle / 2, angle);
                     endAngle = -startAngle;
                 }
-
             }
 
+            // Draw the arc with calculated angles
             arc(cx, cy, radius * 2, radius * 2, startAngle, endAngle);
-        })
-
+        });
     }
 }
 
-
+// Function to redraw the pattern when the mouse is pressed
 function mousePressed() {
     drawBild();
 }
 
-
+// Function to handle window resizing
 function windowResized() {
-    resizeCanvas(windowHeight / PHI, windowHeight);
-    translate(width / 2, height / 2);
-    drawBild();
+    resizeCanvas(windowHeight / PHI, windowHeight); // Resize canvas while maintaining ratio
+    translate(width / 2, height / 2); // Recenter the origin
+    drawBild(); // Redraw the pattern
 }
--- templating_engine.py
+++ templating_engine.py
@@ -23,41 +23,7 @@ DATA_PATH = 'data/data.json'
 env = Environment(loader=FileSystemLoader('templates'))
 
 
-def compile():
-    data = load_data_from_json(DATA_PATH)
-    render_and_save(data)
-    print("Compilation and saving complete.")
-
-
-def render_and_save(data):
-    """Function to render and save the rendered HTML"""
-    # Load the index template
-    index_template = env.get_template('base.html')
-    # Render the index template with data
-    rendered_html = index_template.render(item_list=data)
-
-    # Save the rendered HTML to the root directory as index.html
-    with open('index.html', 'w') as f:
-        f.write(rendered_html)
-
-
-def load_data_from_json(json_file):
-    """Function to load data from JSON file"""
-    with open(json_file, 'r') as f:
-        data = json.load(f)
-    return data
-
-
-class FileChangeHandler(FileSystemEventHandler):
-    """Watchdog event handler"""
-
-    def on_modified(self, event):
-        if event.src_path.endswith('base.html') or event.src_path.endswith('data.json'):
-            print("HTML or JSON file modified. Compiling and saving...")
-            compile()
-
-
-if __name__ == "__main__":
+def main():
     event_handler = FileChangeHandler()
     observer = Observer()
     observer.schedule(event_handler, path='.', recursive=True)
@@ -73,3 +39,41 @@ if __name__ == "__main__":
         observer.stop()
 
     observer.join()
+
+
+def compile():
+    data = load_data_from_json(DATA_PATH)
+    render_and_save(data)
+    print("Compilation and saving complete.")
+
+
+def render_and_save(data):
+    """Render and save the rendered HTML"""
+    # Load the index template
+    index_template = env.get_template('base.html')
+    # Render the index template with data
+    rendered_html = index_template.render(item_list=data)
+
+    # Save the rendered HTML to the root directory as index.html
+    with open('index.html', 'w') as f:
+        f.write(rendered_html)
+
+
+def load_data_from_json(json_file):
+    """Load data from JSON file"""
+    with open(json_file, 'r') as f:
+        data = json.load(f)
+    return data
+
+
+class FileChangeHandler(FileSystemEventHandler):
+    """Watchdog event handler"""
+
+    def on_modified(self, event):
+        if event.src_path.endswith('base.html') or event.src_path.endswith('data.json'):
+            print("HTML or JSON file modified. Compiling and saving...")
+            compile()
+
+
+if __name__ == "__main__":
+    main()
